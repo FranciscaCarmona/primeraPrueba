31b423a370c44785dff0bf693f1b4aaad4b9936e
FranciscaCarmona <49626317+FranciscaCarmona@users.noreply.github.com>
2019-06-24 19:59:22 -0400

    se agregó un comentario

--- src/clase2/Clase2.java
+++ src/clase2/Clase2.java
@@ -6,6 +6,8 @@ public class Clase2 {
 
     
     public static void main(String[] args) {
+    
+        //comentemos algo nuevo
         Ventana v = new Ventana ();
         v.setVisible(true);
         
